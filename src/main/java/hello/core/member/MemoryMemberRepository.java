72db0b77d0735265da21b747d5113435d64324d0
myeongseob1 <kwakms123@ajou.ac.kr>
2022-04-07 20:06:37 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    //실무에서 사용시 동시성 이슈가 있을수 있어 ConcurrentHashMap을 사용한다
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(),member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
