4452ba1b41a331cc447683be33a00a18e8393409
myeongseob1 <kwakms123@ajou.ac.kr>
2022-05-15 22:59:27 +0900

    컴포넌트 스캔 의존관계 자동주입, 수동주입 스캔,필터, 중복등록및 충돌

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component //memoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository {
     //실무에서 사용시 동시성 이슈가 있을수 있어 ConcurrentHashMap을 사용한다
     private static Map<Long, Member> store = new HashMap<>();
